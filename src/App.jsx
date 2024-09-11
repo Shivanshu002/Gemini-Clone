45b7ea8fa461467b3a6a9886a35cd8b31cfe7ab1
Shivanshu002 <shivanshu63073@gmail.com>
2024-09-11 21:36:25 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import Sidebar from './components/sidebar/Sidebar'
 import Main from './components/Main/Main'
+import Sidebar from './components/Sidebarr/Sidebar'
 
 const App = () => {
   return (
